d5e183186297a2a2d5c68fabee718811c5f15052
cwh5278 <cwh5278@206-L04.up.ist.local>
2015-04-20 19:47:46 -0400

    Changes

--- WhackAMole/src/TopPanel.java
+++ WhackAMole/src/TopPanel.java
@@ -1,5 +1,6 @@
 
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -7,6 +8,8 @@ import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.Timer;
+import javax.swing.border.Border;
+import javax.swing.border.LineBorder;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -28,6 +31,8 @@ public class TopPanel extends JPanel implements ActionListener {
     private int timeDelay;
     private Timer timer;
     private JButton start;
+    private int timeLeft;
+    private int scoreTotal;
     
     TopPanel(){
         
@@ -42,17 +47,25 @@ public class TopPanel extends JPanel implements ActionListener {
         time = new JLabel();
         score = new JLabel();
         
+        timeLabel.setFont(new Font("Courier New", Font.BOLD, 32));
+        time.setFont(new Font("Courier New", Font.BOLD, 32));
+        scoreLabel.setFont(new Font("Courier New", Font.BOLD, 32));
+        score.setFont(new Font("Courier New", Font.BOLD, 32));
+        
+        Border border = LineBorder.createBlackLineBorder();//        border.
+        score.setBorder(border);
+        
         start = new JButton("Start");
         this.add(start);
         this.add(timeLabel);
         this.add(scoreLabel);
         this.add(time);
         this.add(score);
-        start.setBounds(new Rectangle(50,50,50,50));
-        timeLabel.setBounds(new Rectangle(100,50,50,50));
-        scoreLabel.setBounds(new Rectangle(50,100,50,50));
-        time.setBounds(new Rectangle(50,50,50,50));
-        score.setBounds(new Rectangle(50,50,50,50));
+        start.setBounds(new Rectangle(0,0,600,50));
+        timeLabel.setBounds(new Rectangle(100,50,100,50));
+        scoreLabel.setBounds(new Rectangle(400,50,120,50));
+        time.setBounds(new Rectangle(100,120,50,50));
+        score.setBounds(new Rectangle(400,120,120,50));
         start.addActionListener(this);
         
     }
@@ -62,8 +75,34 @@ public class TopPanel extends JPanel implements ActionListener {
         Object obj = e.getSource();
         if(obj == start) 
           {
-            
+              timer = new Timer(100, this);             
+              start.setEnabled(false);
+              timeLeft = 45;
+              time.setText(""+timeLeft);
+              timer.start();  
+              scoreTotal = 0;
+              score.setText(""+scoreTotal);
+              
+              //enable game buttons here
+                            
+              
           }
+        
+        if(obj == timer)
+        {
+            if(timeLeft > 0){
+                timeLeft --;
+                time.setText(""+timeLeft);
+            }
+            else{
+                timer.stop();
+                start.setEnabled(true);
+            }
+            if(timeLeft == 20){
+                scoreTotal += 500;
+                score.setText(""+scoreTotal);
+            }
+        }
     }
     
 }
