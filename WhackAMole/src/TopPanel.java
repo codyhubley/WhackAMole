28cd98d0f035a5304c7b1d51f6322e50dbcca38e
cwh5278 <cwh5278@206-L04.up.ist.local>
2015-04-20 18:39:05 -0400

    Changes

--- WhackAMole/src/TopPanel.java
+++ WhackAMole/src/TopPanel.java
@@ -32,19 +32,27 @@ public class TopPanel extends JPanel implements ActionListener {
     TopPanel(){
         
         this.setLayout(null);
-        this.setBackground(Color.red);
+        this.setBackground(Color.white);
         this.setSize(600, 200);
         this.setVisible(true);
         this.setBounds(0, 0, 600, 200);
         
-        timeLabel = new JLabel("Time");
-        scoreLabel = new JLabel();
+        timeLabel = new JLabel("Time:");
+        scoreLabel = new JLabel("Score:");
         time = new JLabel();
         score = new JLabel();
         
         start = new JButton("Start");
         this.add(start);
+        this.add(timeLabel);
+        this.add(scoreLabel);
+        this.add(time);
+        this.add(score);
         start.setBounds(new Rectangle(50,50,50,50));
+        timeLabel.setBounds(new Rectangle(100,50,50,50));
+        scoreLabel.setBounds(new Rectangle(50,100,50,50));
+        time.setBounds(new Rectangle(50,50,50,50));
+        score.setBounds(new Rectangle(50,50,50,50));
         start.addActionListener(this);
         
     }
